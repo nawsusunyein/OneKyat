dfd202eb6084553794377e50aa9729e0b863fa6c
Naw Su Su Nyein <nawsusunyein@gmail.com>
2022-01-28 13:26:54 +0630

    create new project and add necessary files to start a project

---
+++ OneKyat/OneKyat/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  OneKyat
+//
+//  Created by Naw Su Su Nyein on 1/28/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+  
+
+}
+
---
+++ OneKyat/OneKyat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OneKyat
+//
+//  Created by Naw Su Su Nyein on 1/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
